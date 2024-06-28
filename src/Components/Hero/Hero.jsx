a3854f21ec80828137fbc3d833d851d9b16c705f
Angxu-iQueue <97353594+Angxu-iQueue@users.noreply.github.com>
2024-06-28 16:33:27 +0530

    ui change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,33 +1,25 @@
-import React from 'react'
-import './Hero.css'
+import React from 'react';
+import './Hero.css';
 import { Carousel } from 'antd';
 import ITEMS from './carousel';
-
 
 const Hero = () => {
   return (
     <div className='Hero'>
-      
       <div className="carousel-block">
-      <p className='carousel-title'>Your Local Vendor <br/> Now At Your doorstep</p>
-      <Carousel autoplay autoplaySpeed={5000}> 
-      { 
-        ITEMS.map((ITEMS) =>{
-        return (
-          <div className='carousel-card'>
-            <div className="carousel-img">
-              <li key={ITEMS.key}>
-            <img src={ITEMS.pic} alt= {ITEMS.key} />
-              </li>
+        <p className='carousel-title'>Your Local Vendor <br/> Now At Your doorstep</p>
+        <Carousel autoplay autoplaySpeed={5000} dotPosition="bottom">
+          {ITEMS.map((item) => (
+            <div className='carousel-card' key={item.key}>
+              <div className="carousel-img">
+                <img className='hero-img' src={item.pic} alt={item.key} />
+              </div>
             </div>
-          </div>
-        );
-
-      })}
-      </Carousel>
+          ))}
+        </Carousel>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
